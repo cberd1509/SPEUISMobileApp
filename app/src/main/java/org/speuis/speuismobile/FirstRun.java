03772ced97b525452093e0645229060e004597de
Carlos Andrés <cberd1509@gmail.com>
2017-05-28 16:49:11 -0500

    Error para desinstalar :/

--- app/src/main/java/org/speuis/speuismobile/FirstRun.java
+++ app/src/main/java/org/speuis/speuismobile/FirstRun.java
@@ -1,10 +1,19 @@
 package org.speuis.speuismobile;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.onClick;
 
 public class FirstRun extends AppCompatActivity {
 
@@ -13,7 +22,7 @@ public class FirstRun extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_run);
 
-        Typeface Exo2Light = Typeface.createFromAsset(getAssets(),"fonts/Exo2-Light.otf");
+        Typeface Exo2Light = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Light.otf");
         TextView tvBienvenido = (TextView) findViewById(R.id.tvBienvenido);
         TextView tvMesage1 = (TextView) findViewById(R.id.tvMessage1);
         Button BotonInicio = (Button) findViewById(R.id.btContinue);
@@ -22,6 +31,7 @@ public class FirstRun extends AppCompatActivity {
         tvBienvenido.setTypeface(Exo2Light);
         BotonInicio.setTypeface(Exo2Light);
 
-
     }
+
+
 }
