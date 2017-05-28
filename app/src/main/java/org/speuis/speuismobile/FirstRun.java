6cb82e3fbacf0aebf875a6e038e9d429a1fdcec2
Carlos Andrés <cberd1509@gmail.com>
2017-05-28 16:04:27 -0500

    Terminado el Layout de la Primera Activity

--- app/src/main/java/org/speuis/speuismobile/FirstRun.java
+++ app/src/main/java/org/speuis/speuismobile/FirstRun.java
@@ -1,7 +1,10 @@
 package org.speuis.speuismobile;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FirstRun extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class FirstRun extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_run);
+
+        Typeface Exo2Light = Typeface.createFromAsset(getAssets(),"fonts/Exo2-Light.otf");
+        TextView tvBienvenido = (TextView) findViewById(R.id.tvBienvenido);
+        TextView tvMesage1 = (TextView) findViewById(R.id.tvMessage1);
+        Button BotonInicio = (Button) findViewById(R.id.btContinue);
+
+        tvMesage1.setTypeface(Exo2Light);
+        tvBienvenido.setTypeface(Exo2Light);
+        BotonInicio.setTypeface(Exo2Light);
+
+
     }
 }
